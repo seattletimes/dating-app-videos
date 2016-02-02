e8e377921c86d86a24b21330b2f8bf45c8e36829
Thomas Wilburn <news@thomaswilburn.net>
2016-02-01 16:16:37 -0800

    Question tweaks

--- src/js/questions.js
+++ src/js/questions.js
@@ -1,17 +1,17 @@
 module.exports = [
   {
-    headline: "I have dated online before...",
-    left: { text: "Yes", value: 1, video: 112 },
-    right: { text: "No", value: 2, video: 221 }
+    headline: "Have you used online dating sites?",
+    left: { text: "Yes", id: "4721524007001" },
+    right: { text: "No", id: "4721533281001" }
   },
   {
-    headline: "This space intentionally left blank.",
-    left: { text: "A", value: "a" },
-    right: { text: "B", value: "b" }
+    headline: "What would you rather do?",
+    left: { text: "Work hard", id: "4721524008001" },
+    right: { text: "Play hard", id: "4721531072001" }
   },
   {
-    headline: "This space intentionally left blank.",
-    left: { text: "A", value: "a" },
-    right: { text: "B", value: "b" }
+    headline: "What's your dating style?",
+    left: { text: "Unlimited options", id: "4721524009001" },
+    right: { text: "Careful curation", id: "4721533283001" }
   }
 ];
