41e46f1bb41687f6c57eb742259a68f33ab2bdee
Thomas Wilburn <news@thomaswilburn.net>
2016-02-02 15:35:59 -0800

    Switch to ad-less player

--- src/js/brightcove.js
+++ src/js/brightcove.js
@@ -1,7 +1,7 @@
 var callbacks = [];
 var player;
 
-var src="//players.brightcove.net/1509317113/default_default/index.min.js";
+var src="//players.brightcove.net/1509317113/VkZ4EROte_default/index.min.js";
 var script = document.createElement("script");
 script.src = src;
 script.onload = function() {
